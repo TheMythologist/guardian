e71d857b13eae9dd5e6e34cc9d2db5d22a733599
TheMythologist <leekaixuan2001@gmail.com>
2023-01-24 19:51:58 +0800

    feat: speed improvements for IPv4 address validation (#10)

--- util/validator.py
+++ util/validator.py
@@ -1,4 +1,3 @@
-import ipaddress
 import re
 import socket
 
@@ -16,7 +15,6 @@ class NameInCustom(Validator):
     def validate(self, document):
         global custom_ips
         if custom_ips.has(document.text):
-            # Move cursor to end
             raise ValidationError(
                 message="Name already in list", cursor_position=len(document.text)
             )
@@ -26,7 +24,6 @@ class NameInBlacklist(Validator):
     def validate(self, document):
         global blacklist
         if blacklist.has(document.text):
-            # Move cursor to end
             raise ValidationError(
                 message="Name already in list", cursor_position=len(document.text)
             )
@@ -34,45 +31,43 @@ class NameInBlacklist(Validator):
 
 class IPValidator(Validator):
     def validate(self, document):
-        # Move cursor to end
         error = ValidationError(
             message="Not a valid IP or URL", cursor_position=len(document.text)
         )
         try:
             ip = document.text
             if ipv4.match(ip):
-                ipaddress.IPv4Address(ip)
+                socket.inet_aton(ip)
             elif not domain.match(ip):
                 raise error
-        except (ipaddress.AddressValueError, socket.gaierror):
+        except socket.error:
             raise error
 
     # TODO: Add an extra validator to check if an IP could be used by R* services (i.e. it's part of Microsoft Azure)
 
     @staticmethod
     def validate_get(text):
-        # Move cursor to end
         error = ValidationError(
             message="Not a valid IP or URL", cursor_position=len(text)
         )
         try:
             ip = text
             if ipv4.match(ip):
-                ipaddress.IPv4Address(ip)
+                socket.inet_aton(ip)
             elif domain.match(ip):
-                ip = socket.gethostbyname(text)
-                ipaddress.IPv4Address(ip)
+                try:
+                    ip = socket.gethostbyname(text)
+                except socket.gaierror:
+                    raise ValidationError(
+                        message=f"URL {text} can't be resolved to IP",
+                        cursor_position=len(text),
+                    )
+                socket.inet_aton(ip)
             else:
                 raise error
             return ip
-        except ipaddress.AddressValueError:
+        except socket.error:
             raise error
-        except socket.gaierror:
-            # Move cursor to end
-            raise ValidationError(
-                message=f"URL {text} can't be resolved to IP",
-                cursor_position=len(text),
-            )
 
 
 class IPInCustom(IPValidator):
@@ -80,7 +75,6 @@ class IPInCustom(IPValidator):
         super().validate(document)
         global custom_ips
         if document.text in custom_ips or custom_ips.has(document.text, "value"):
-            # Move cursor to end
             raise ValidationError(
                 message="IP already in list", cursor_position=len(document.text)
             )
@@ -91,7 +85,6 @@ class IPInBlacklist(Validator):
         super().validate(document)
         global blacklist
         if document.text in blacklist or blacklist.has(document.text, "value"):
-            # Move cursor to end
             raise ValidationError(
                 message="IP already in list", cursor_position=len(document.text)
             )
@@ -101,14 +94,12 @@ class ValidateToken(Validator):
     def validate(self, document):
         conn = networkmanager.Cloud(document.text)
         if not conn.check_connection():
-            # Move cursor to end
             raise ValidationError(
                 message="DigitalArc is unavailable, unable to check token",
                 cursor_position=len(document.text),
             )
 
         if not conn.check_token():
-            # Move cursor to end
             raise ValidationError(
                 message="Token invalid", cursor_position=len(document.text)
             )
