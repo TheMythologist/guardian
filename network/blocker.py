211416a907e0cb34b8176963bceebd3b9729b408
TheMythologist <leekaixuan2001@gmail.com>
2023-01-27 23:06:21 +0800

    fix: standardise logging format in debug logs

--- network/blocker.py
+++ network/blocker.py
@@ -18,7 +18,7 @@ if not debug_logger.handlers:
     fh.setLevel(logging.DEBUG)
     fh.setFormatter(
         logging.Formatter(
-            "%(asctime)s|%(levelname)s: %(message)s", datefmt="%Y-%m-%d %H:%M:%S"
+            "[%(asctime)s][%(levelname)s] %(message)s", datefmt="%Y-%m-%d %H:%M:%S"
         )
     )
     debug_logger.addHandler(fh)
