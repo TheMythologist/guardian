3ae2e7487b3b1ea556404572f7a4b95f7597576c
TheMythologist <leekaixuan2001@gmail.com>
2023-01-31 11:22:35 +0800

    refactor: clean up imports

--- src/util/DynamicBlacklist.py
+++ src/util/DynamicBlacklist.py
@@ -1,4 +1,3 @@
-import contextlib
 import json
 import os.path
 import re
@@ -7,7 +6,7 @@ import time
 import prsw
 import requests
 
-from util.constants import CIDR_MASKS
+from util.network import construct_cidr_block_set
 
 # This file contains classes and methods to manage acquiring, parsing, and updating a possibly dynamic list of IP ranges
 # that Guardian needs to be aware of. Such ranges include R* / T2 official IPs, as well as IPs that can be used for
@@ -189,43 +188,6 @@ def parse_azure_ip_ranges_from_file(location_of_file: str) -> list[str]:
         return parse_azure_ip_ranges(file.read())
 
 
-def calculate_ip_to_int(ip: str) -> int:
-    octets = [int(num) for num in ip.split(".")]
-    # Manually perform calculation for speed purposes
-    return (
-        octets[0] * (2**24) + octets[1] * (2**16) + octets[2] * (2**8) + octets[3]
-    )
-
-
-def cidr_to_tuple(ip_in_cidr: str) -> tuple[int, int]:
-    """
-    Converts a string representing an IP in CIDR notation to two integers,
-    the first integer represents the lowest IP in the CIDR block,
-    and the second integer represents the mask (just the suffix)
-
-    NOTE: Does *not* check for the validity of a CIDR block. Example, 255.255.255.255/1 would be accepted, but is not
-    a valid CIDR block.
-    """
-    ip, suffix = ip_in_cidr.split("/")
-    suffix_int = int(suffix)
-    return calculate_ip_to_int(ip), suffix_int
-
-
-def construct_cidr_block_set(ips_in_cidr: list[str]) -> set[tuple[int, int]]:
-    """
-    Construct a set of IPs in CIDR notation. This set is specifically optimised to only work with the
-    ip_in_cidr_block_set() function.
-
-    Ignores any element which is not valid IPv4 CIDR notation (as long as it was still a string).
-    """
-    ip_set = set()
-    for ip_cidr in ips_in_cidr:
-        with contextlib.suppress(ValueError):
-            # [0] is IP as integer, [1] is subnet mask in /xy notation (only xy)
-            ip_set.add(cidr_to_tuple(ip_cidr))
-    return ip_set
-
-
 def get_dynamic_blacklist(backup_file: str = "db.json") -> set[tuple[int, int]]:
     # TODO: It seems like we can determine if a range has changed by looking at the 'changeNumber' attribute
     # for a given category, however, there unfortunately doesn't appear to be any sort of timestamp included
@@ -251,17 +213,6 @@ def get_dynamic_blacklist(backup_file: str = "db.json") -> set[tuple[int, int]]:
             ) from e
         ranges = parse_azure_ip_ranges_from_file(backup_file)
     return construct_cidr_block_set(ranges)
-
-
-def ip_in_cidr_block_set(ip: str, cidr_block_set, min_cidr_suffix: int = 0) -> bool:
-    """
-    Essentially a reverse-search for all possible entries in cidr_block_set that would contain ip.
-    """
-    ip_int = calculate_ip_to_int(ip)
-    for suffix in range(len(CIDR_MASKS)):
-        if (ip_int & CIDR_MASKS[suffix], suffix) in cidr_block_set:
-            return True
-    return False
 
 
 if __name__ == "__main__":
--- src/util/network.py
+++ src/util/network.py
@@ -1,6 +1,9 @@
+import contextlib
 import socket
 
 import requests
+
+from util.constants import CIDR_MASKS
 
 
 def get_public_ip() -> str:
@@ -13,3 +16,51 @@ def get_private_ip():
     local_ip = soc.getsockname()[0]
     soc.close()
     return local_ip
+
+
+def calculate_ip_to_int(ip: str) -> int:
+    octets = [int(num) for num in ip.split(".")]
+    # Manually perform calculation for speed purposes
+    return (
+        octets[0] * (2**24) + octets[1] * (2**16) + octets[2] * (2**8) + octets[3]
+    )
+
+
+def ip_in_cidr_block_set(ip: str, cidr_block_set, min_cidr_suffix: int = 0) -> bool:
+    """
+    Essentially a reverse-search for all possible entries in cidr_block_set that would contain ip.
+    """
+    ip_int = calculate_ip_to_int(ip)
+    for suffix in range(len(CIDR_MASKS)):
+        if (ip_int & CIDR_MASKS[suffix], suffix) in cidr_block_set:
+            return True
+    return False
+
+
+def cidr_to_tuple(ip_in_cidr: str) -> tuple[int, int]:
+    """
+    Converts a string representing an IP in CIDR notation to two integers,
+    the first integer represents the lowest IP in the CIDR block,
+    and the second integer represents the mask (just the suffix)
+
+    NOTE: Does *not* check for the validity of a CIDR block. Example, 255.255.255.255/1 would be accepted, but is not
+    a valid CIDR block.
+    """
+    ip, suffix = ip_in_cidr.split("/")
+    suffix_int = int(suffix)
+    return calculate_ip_to_int(ip), suffix_int
+
+
+def construct_cidr_block_set(ips_in_cidr: list[str]) -> set[tuple[int, int]]:
+    """
+    Construct a set of IPs in CIDR notation. This set is specifically optimised to only work with the
+    ip_in_cidr_block_set() function.
+
+    Ignores any element which is not valid IPv4 CIDR notation (as long as it was still a string).
+    """
+    ip_set = set()
+    for ip_cidr in ips_in_cidr:
+        with contextlib.suppress(ValueError):
+            # [0] is IP as integer, [1] is subnet mask in /xy notation (only xy)
+            ip_set.add(cidr_to_tuple(ip_cidr))
+    return ip_set
